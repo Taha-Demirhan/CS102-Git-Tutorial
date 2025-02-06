1be2913c2635c0eebee1029e2a649a39872201bc
Taha Demirhan <taha.demirhan-ug@labs.bilkent.edu.tr>
2025-02-06 09:51:59 +0300

    lack parts are completed.

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -26,7 +26,10 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        replaceChar('a',true);
+        for (int i = 0; i < inputString.length(); i++)   
+        {
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
         
         return outputString;
     }
@@ -43,6 +46,7 @@ public class Cipher
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
 
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
@@ -50,6 +54,7 @@ public class Cipher
             for (int i = 0; i < CIPHER_ALPHABET.length(); i++)   
             {
                 if(CIPHER_ALPHABET.charAt(i) == inputChar) {
+
                     return ORIGINAL_ALPHABET.charAt(i);
                 }
             }
